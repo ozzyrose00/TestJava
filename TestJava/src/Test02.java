ea235cac379fc334fc24ccc75128fb1e8d514c01
ozzyrose00 <ozzyrose00@gasystem.co.kr>
2025-04-10 10:05:30 +0900

    test

--- TestJava/src/Test02.java
+++ TestJava/src/Test02.java
@@ -4,6 +4,8 @@ public class Test02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("EEEEEEEEEEEEEEEEEEEEEE");
+		System.out.println("EEEEEEEEEEEEEEEEEEEEEE");
+		System.out.println("EEEEEEEEEEEEEEEEEEEEEE");
 	}
 
 }
